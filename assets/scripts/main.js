bf1ab9d0624061aece1af1d3f91ac9cfa34c1a10
Nathan Karter <nathanielkarter@gmail.com>
2022-11-05 18:14:53 -0700

    expose
    Co-authored-by: Hariz Zariman <hzariman@users.noreply.github.com>

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -24,6 +24,13 @@ function getRecipesFromStorage() {
   // A9. TODO - Complete the functionality as described in this function
   //           header. It is possible in only a single line, but should
   //           be no more than a few lines.
+  let recipes = localStorage.getItem('recipes');
+  let recipeArray = JSON.parse(recipes);
+  if (recipeArray == null) {
+    return [];
+  }
+  
+  return recipeArray;
 }
 
 /**
@@ -35,10 +42,16 @@ function getRecipesFromStorage() {
  */
 function addRecipesToDocument(recipes) {
   // A10. TODO - Get a reference to the <main> element
+  let mainEl = document.querySelector('main');
   // A11. TODO - Loop through each of the recipes in the passed in array,
   //            create a <recipe-card> element for each one, and populate
   //            each <recipe-card> with that recipe data using element.data = ...
   //            Append each element to <main>
+  for(let i = 0; i < recipes.length; i++){
+    let recipeCard = document.createElement('recipe-card');
+    recipeCard.data = recipes[i];
+    mainEl.append(recipeCard);
+  }
 }
 
 /**
